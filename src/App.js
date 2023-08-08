b56d8fe9213f0e7c844d42d5476ee111447082c3
Jugal Kishor <Jugalkishor55645@gmail.com>
2023-08-08 10:23:33 +0530

    change3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Home/>
      <Footer/>
     </div>
   );
---
+++ src/Components/Home.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+function Home() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_nlm1x3f', 'template_hi3xzic', form.current, 'tqHSqObCYK-dmzwWU')
+        .then((result) => {
+            console.log(result.text);
+            console.log("Massage send");
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+            <center>Mail Form</center>
+            <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" className='form-control' name="user_name" />
+      <label>Email</label>
+      <input type="email" className='form-control' name="user_email" />
+      <label>Message</label>
+      <textarea className='form-control' name="message" />
+      <input type="submit" className='form-control' value="Send" />
+    </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
